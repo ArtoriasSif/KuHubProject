7110a477c29da3f1047c6668bf60889763170557
Matheus Lara <ma.delara@duocuc.cl>
2025-08-05 21:30:06 -0400

    Commit msvc Solicitud Docente.

---
+++ msvc-SolicitudDocente/src/main/java/msvc_SolicitudDocente/msvc_SolicitudDocente/MsvcSolicitudDocenteApplication.java
@@ -0,0 +1,15 @@
+package msvc_SolicitudDocente.msvc_SolicitudDocente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class MsvcSolicitudDocenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcSolicitudDocenteApplication.class, args);
+	}
+
+}
---
+++ msvc-SolicitudDocente/src/test/java/msvc_SolicitudDocente/msvc_SolicitudDocente/MsvcSolicitudDocenteApplicationTests.java
@@ -0,0 +1,13 @@
+package msvc_SolicitudDocente.msvc_SolicitudDocente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcSolicitudDocenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
