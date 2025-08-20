722932d3f3c84ea4acdec5886f81b071e5398c0e
Matheus Lara <ma.delara@duocuc.cl>
2025-08-19 22:25:39 -0400

    Implementación msvc-receta y detalle receta, @GetMapping y Post

---
+++ msvc-DetalleReceta/src/test/java/DetalleReceta/msvc_DetalleReceta/MsvcDetalleRecetaApplicationTests.java
@@ -0,0 +1,13 @@
+package DetalleReceta.msvc_DetalleReceta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcDetalleRecetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ msvc-Receta/src/test/java/Recetas/msvc_Recetas/MsvcRecetasApplicationTests.java
@@ -0,0 +1,13 @@
+package Recetas.msvc_Recetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcRecetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
