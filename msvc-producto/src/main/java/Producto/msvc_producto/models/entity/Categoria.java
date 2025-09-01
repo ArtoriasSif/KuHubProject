f908be44940d99fffe5192d3c441f2cf3db5f051
FranciscoGomezRa <franc.gomezr@duocuc.cl>
2025-08-31 23:53:16 -0400

    -

--- msvc-producto/src/main/java/Producto/msvc_producto/models/entity/Categoria.java
+++ msvc-producto/src/main/java/Producto/msvc_producto/models/entity/Categoria.java
@@ -1,5 +1,6 @@
 package Producto.msvc_producto.models.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -12,6 +13,7 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_categoria", nullable=false)
+    //@JsonIgnore
     private Long idCategoria;
 
     @Column(name= "nombre_categoria")
